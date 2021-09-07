ed1c1730be1787aeadee9e1e965a4c168e7351b0
Damian De Leon <damianmdeleon@gmail.com>
2021-09-07 16:18:58 -0500

    ompleted Lesson2

---
+++ tech-news-java-api/src/test/java/com/technews/VariableTypes.java
@@ -0,0 +1,13 @@
+package com.technews;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        String name = "Damian";
+        int age = 1976 + 45;
+        double price = 19.99;
+        boolean truth = true;
+        boolean falseness = false;
+
+        System.out.println(price);
+    }
+}
---
+++ tech-news-java-api/src/test/java/com/technews/VariablesPractice.java
@@ -0,0 +1,18 @@
+package com.technews;
+
+public class VariablesPractice {
+    public static void main(String[] args){
+        int a = 4;
+        double b = 16.99;
+        double sum = a + b;
+        System.out.println(sum);
+// sum has to be a double since it contains a decimal
+
+        String line = "My age is...";
+        String newLine = line + a;
+        System.out.println(newLine);
+//        newLine has to be a String because it has words
+
+
+    }
+}
